2097fc0c4de261900474e154089a78db6969124d
parkhye@hanmail.net <kiki10041245@>
2022-02-23 21:30:55 +0900

    박혜경 - 프로젝트 환경설정

--- src/main/java/com/somsom/cyworld/HelloCotroller.java
+++ src/main/java/com/somsom/cyworld/HelloCotroller.java
@@ -1,12 +1,13 @@
-package com.somsom.cyworld.hello;
+package com.somsom.cyworld;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class helloController {
-	 	@ResponseBody
+public class HelloCotroller {
+
+		@ResponseBody
 	    @RequestMapping("/hello")
 	    public String helloWorld() {
 	        return "Hello World!!";
