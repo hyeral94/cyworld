91f88d14338feddfc4416deb028571b0e0d3ff94
parkhye@hanmail.net <kiki10041245@>
2022-02-23 21:19:24 +0900

    박혜경 - cyworld 프로젝트 생성

---
+++ src/test/java/com/somsom/cyworld/CyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somsom.cyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
