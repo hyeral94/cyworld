269f1f0afe12b3f5891e62c3f85b1aba2aa88829
parkhye@hanmail.net <kiki10041245@>
2022-03-28 19:22:51 +0900

    박혜경 - 싸이월드 이미지 업로드 수정

--- src/main/java/com/somsom/cyworld/post/setting/dao/SettingDAO.java
+++ src/main/java/com/somsom/cyworld/post/setting/dao/SettingDAO.java
@@ -25,6 +25,9 @@ public interface SettingDAO {
 	
 	//프로필 이미지 설정 값 가져오기
 	public SettingProfileImage selectProfileImage(@Param("userId") int userId);
+	
+	// 현재 로그인 접속자가 미니홈피 주인인지 아닌지 구분
+	public int selectConnectUserId(int userId);
 
 }
  
