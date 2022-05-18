637a280184d3bfb95e41145819fc358b2b3851be
DESKTOP-5NFCBAS\parkh <parkhye@hanmail.net>
2022-05-18 17:35:26 +0900

    (박혜경) - 파일 경로 수정

--- src/main/java/com/somsom/cyworld/common/FileManagerService.java
+++ src/main/java/com/somsom/cyworld/common/FileManagerService.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
 	
-	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/upload_images/";
+	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/cyworld/upload_images/";
 
 //	public final static String FILE_UPLOAD_PATH = "C:\\Users\\parkh\\Documents\\spring project\\upload\\image/";
 
